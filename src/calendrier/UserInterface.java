f502b4d9be36b6dca77056dfd167a424c39936d8
Yee Hui Poh <yhpoh91@gmail.com>
2015-10-01 12:23:49 +0800

    Added comments

--- src/calendrier/UserInterface.java
+++ src/calendrier/UserInterface.java
@@ -1,8 +1,20 @@
 package calendrier;
 
 import utils.Event;
+import java.util.List;
+import java.util.ArrayList;
 
 public class UserInterface {
+	private MainLogic mainLogic = null;
+	
+	public UserInterface(MainLogic mainLogic){
+		this.mainLogic = mainLogic;
+	}
+	
+	/**
+	 * Notifies user that event is starting soon 
+	 * @param event event that is starting soon
+	 */
 	public void notifyUser(Event event){
 		
 	}
